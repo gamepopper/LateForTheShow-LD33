78418220f15c0c0c36557d0d0c9a32e5c527a543
Tim Stoddard <info@gamepopper.co.uk>
2015-08-23 16:02:44 +0100

    Included Ending and Timer

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,8 +6,10 @@ import flixel.FlxState;
 import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxRandom;
 import flixel.math.FlxVector;
+import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import flixel.util.FlxSort;
+import flixel.util.FlxTimer;
 
 /**
  * A FlxState which can be used for the actual gameplay.
@@ -18,6 +20,10 @@ class PlayState extends FlxState
 	var player:Player;
 	var objectGroup:FlxSpriteGroup = new FlxSpriteGroup();
 	var progress:CostumeProgess;
+	
+	var time:Int = 15;
+	var text:FlxText;
+	var countdown:FlxTimer = new FlxTimer();
 	
 	var random:FlxRandom = new FlxRandom();
 	
@@ -34,6 +40,13 @@ class PlayState extends FlxState
 	{
 		super.create();
 		
+		Reg.GotBody = false;
+		Reg.GotFeet = false;
+		Reg.GotHands = false;
+		Reg.GotHead = false;
+		Reg.GotTail = false;
+		Reg.ItemCount = 0;
+		
 		FlxG.debugger.drawDebug = true;
 		
 		FlxG.camera.bgColor = FlxColor.WHITE;
@@ -47,20 +60,31 @@ class PlayState extends FlxState
 		progress.setPosition(130, 0);
 		
 		head.loadGraphic("assets/images/Monsters/Monster" + 1 + "Small_Head.png");
+		head.offset.y = -5;
 		add(head);
 		PlaceCostume("head", head);
 		body.loadGraphic("assets/images/Monsters/Monster" + 1 + "Small_Body.png");
+		body.offset.y = 5;
 		add(body);
 		PlaceCostume("body", body);
 		hand.loadGraphic("assets/images/Monsters/Monster" + 1 + "Small_Hands.png");
+		hand.offset.y = -1;
 		add(hand);
 		PlaceCostume("hands", hand);
 		feet.loadGraphic("assets/images/Monsters/Monster" + 1 + "Small_Feet.png");
+		feet.offset.y = 8;
 		add(feet);
 		PlaceCostume("feet", feet);
 		tail.loadGraphic("assets/images/Monsters/Monster" + 1 + "Small_Tail.png");
+		tail.offset.y = 5;
 		add(tail);
 		PlaceCostume("tail", tail);
+		
+		text = new FlxText(5, 0, FlxG.width, Std.string(time));
+		text.color = FlxColor.BLACK;
+		add(text);
+		
+		countdown.start(1, CountdownHit, 1);
 	}
 
 	/**
@@ -69,6 +93,12 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		
+		if (player.y > 120)
+		{
+			FlxG.switchState(new EndingState(Reg.ItemCount == 0 ? NOCOSTUME : 
+				Reg.ItemCount < 5 ? MISSINGPARTS : COMPLETE));
+		}
 		
 		room.update(elapsed);
 		objectGroup.sort(FlxSort.byY);
@@ -86,5 +116,20 @@ class PlayState extends FlxState
 			room.hidingAreas[hidingID].position.y);
 		sprite.visible = room.hidingAreas[hidingID].visible;
 		room.hidingAreas[hidingID].sprite = sprite;
+	}
+	
+	function CountdownHit(timer:FlxTimer)
+	{
+		time--;
+		
+		if (time > 0)
+		{
+			text.text = Std.string(time);
+			countdown.start(1, CountdownHit, 1);
+		}
+		else
+		{
+			FlxG.switchState(new EndingState(LATE));
+		}
 	}
 }
