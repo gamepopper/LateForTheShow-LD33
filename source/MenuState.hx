20b6c67534a6e8f67cbd6ce06361c109c2422274
Tim Stoddard <info@gamepopper.co.uk>
2015-08-23 20:26:01 +0100

    Updated Menu

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,15 +1,47 @@
 package;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 
 /**
  * A FlxState which can be used for the game's menu.
  */
 class MenuState extends FlxState
-{	
+{
+	var start:FlxText;
+	var lyserdigi:FlxText;
+	
+	var logo:FlxSprite = new FlxSprite();
+	
+	var flash:FlxTimer = new FlxTimer();
+	
 	/**
 	 * Function that is called up when to state is created to set it up.
 	 */
 	override public function create():Void
 	{
+		FlxG.debugger.drawDebug = true;
+		FlxG.camera.bgColor = FlxColor.WHITE;
+		
+		start = new FlxText(0, 80, FlxG.width, "SPACE/ENTER to START", 8);
+		start.color = FlxColor.BLACK;
+		start.alignment = "center";
+		add(start);
+		
+		lyserdigi = new FlxText(0, 105, FlxG.width, "Music by Lyserdigi", 8);
+		lyserdigi.color = FlxColor.BLACK;
+		lyserdigi.alignment = "right";
+		add(lyserdigi);
+		
+		logo.loadGraphic("assets/images/logo.png");
+		logo.setPositionUsingCenter(80, 40);
+		add(logo);
+		
+		flash.start(0.5, flashCallback, 0);
+		
 		super.create();
 	}
 
@@ -27,6 +59,16 @@ class MenuState extends FlxState
 	 */
 	override public function update(elapsed:Float):Void
 	{
+		if (FlxG.keys.anyJustPressed(["SPACE", "ENTER"]))
+		{
+			FlxG.switchState(new HowToPlayState());
+		}
+		
 		super.update(elapsed);
+	}
+	
+	function flashCallback(timer:FlxTimer)
+	{
+		start.visible = !start.visible;
 	}
 }
